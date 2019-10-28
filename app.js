fd6311f4b86f5a4ae21039ddc8e0a1c87fcd9720
jonnowelch <jonnowelch@yahoo.com>
2019-10-28 14:24:37 +0000

    cors added

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api-router');
+const cors = require('cors');
 const {
   handleCustomErrors,
   handlePsqlErrors,
   handleServerError
 } = require('./errors/errors');
 
-const cors = require('cors');
 app.use(cors());
 
 app.use(express.json());
@@ -23,7 +23,3 @@ app.use(handleCustomErrors);
 app.use(handleServerError);
 
 module.exports = app;
-
-// https://dashboard.heroku.com/apps/nc-news-jonno/resources?justInstalledAddonServiceId=6c67493d-8fc2-4cd4-9161-4f1ec11cbe69
-
-// postgres://mbdrmvndzqbnqq:0f092dc04083ed1ee41b6cf77b3e2bec703a8f716ad56233df0966c286a99a43@ec2-46-137-187-23.eu-west-1.compute.amazonaws.com:5432/d6923eku2m0ud
